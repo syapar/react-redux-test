bf2b6456af16a560c09af43eb9be462f5e586d64
Serhat Yapar <yaparserhat@gmail.com>
2018-03-26 21:14:55 +0100

    LESS Configuration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './containers/app/App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('root'));
