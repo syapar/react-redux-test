45c9b92612015881630b08238ea0c471829813a3
Serhat Yapar <yaparserhat@gmail.com>
2018-03-27 22:26:48 +0100

    Paging Component

---
+++ src/components/paging/PagingButton.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const PagingButton = ({buttonText, page, onPageClicked, isCurrentPage}) => {
+
+	const onButtonClicked = (e) => {
+		e.preventDefault();
+
+		if(!isCurrentPage){
+			onPageClicked(page);
+		}
+	};
+
+	return(
+		<a
+			className={`paging__button ${ isCurrentPage ? 'paging__button--current':''}`}
+			onClick={onButtonClicked}>
+			{buttonText}
+		</a>
+	);
+}
+
+
+
+export default PagingButton;
